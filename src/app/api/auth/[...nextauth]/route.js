905150fa01aef1ff6ba3ab0344cab704d3137f06
Alejandro Zepeda <alexzepeda07@hotmail.com>
2023-11-25 00:01:35 -0600

    Create GithubProvider with clientId & clientSecret

---
+++ src/app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,28 @@
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+
+const authOptions = {
+    providers: [
+        GithubProvider({
+            clientId: 'Iv1.1477fea00c5ea43e',
+            clientSecret: '2167b25baaca6410741890effcafd317bcdbd258'
+        })
+    ],
+    callbacks : {
+        async session({session, token, user}) {
+            session.user.username = session?.user?.name
+                .split(" ")
+                .join("")
+                .toLocaleLowerCase();
+
+            session.user.uid = token.sub;
+
+            return session;
+        }
+    },
+    secret: "default_secret_key"
+};
+
+const handler = NextAuth(authOptions)
+
+export { handler as GET, handler as POST }
