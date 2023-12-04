154ec1bcc2604b7985f7e987a1620580bc7643db
Alejandro Zepeda <alexzepeda07@hotmail.com>
2023-12-03 22:16:51 -0600

    Add manage account to index browser

--- src/app/browse/page.js
+++ src/app/browse/page.js
@@ -1,15 +1,21 @@
 'use client'
 
+import ManageAccounts from "@/components/manage-account"
 import UnauthPage from "@/components/unauth-page";
+import { GlobalContext } from "@/context";
 import { useSession } from "next-auth/react"
+import { useContext } from "react";
 
 export default function Browse() {
+
+    const {loggedInAccount} = useContext(GlobalContext);
 
     const { data: session } = useSession();
 
     console.log(session, 'session');
 
     if (session === null) return <UnauthPage/>
+    if (loggedInAccount === null) return <ManageAccounts />
 
     return <div>Browse</div>
 }
--- src/context/index.js
+++ src/context/index.js
@@ -2,15 +2,21 @@
 
 import CircleLoader from "@/components/circle-loader";
 import { useSession } from 'next-auth/react';
-import { createContext } from 'react';
+import { createContext, useState } from 'react';
 
 export const GlobalContext = createContext(null);
 
 export default function GlobalState({ children }) {
 
+    const [loggedInAccount, setLoggedInAccount] = useState(null);
+
     const {data: session} = useSession();
 
     if (session === undefined) return <CircleLoader/>
 
-    return <GlobalContext.Provider value={{}}>{children}</GlobalContext.Provider>
+    return (
+        <GlobalContext.Provider value={{loggedInAccount, setLoggedInAccount}}>
+            {children}
+        </GlobalContext.Provider>
+    )
 }
