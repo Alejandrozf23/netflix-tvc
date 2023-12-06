731dff83f3a80d4db6c71cbcae747dd1628d240a
Alejandro Zepeda <alexzepeda07@hotmail.com>
2023-12-05 22:28:55 -0600

    Add async function getAllAccounts with fetch request to manage account

--- src/components/manage-account/index.js
+++ src/components/manage-account/index.js
@@ -1,5 +1,34 @@
 'use client'
 
+import { GlobalContext } from "@/context"
+import { useSession } from "next-auth/react";
+import { useContext } from "react"
+
 export default function ManageAccounts() {
-    return <div>ManageAccounts</div>
+
+    const { account, setAccounts } = useContext(GlobalContext);
+
+    const { data: session } = useSession();
+
+    async function getAllAccounts() {
+        const response = await fetch(
+            `/api/account/get-all-accounts?id=${session?.user?.uid}`,
+            {method: "GET"}
+        );
+
+        const data = await response.json();
+        
+        return data;
+    }
+
+    return <div className="min-h-screen flex justify-center flex-col items-center relative">
+        <div className="flex justify-center flex-col items-center">
+            <h1 className="text-white font-bold text-[54px] my-[36px]">
+                Who's Watching?
+            </h1>
+            <ul>
+                
+            </ul>
+        </div>        
+    </div>
 }
