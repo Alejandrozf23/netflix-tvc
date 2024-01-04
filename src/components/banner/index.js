c5108a75253670884dbf72390aab5da6102c36b9
Alejandro Zepeda <alexzepeda07@hotmail.com>
2024-01-03 23:43:41 -0600

    Fix show image, pending fix properties of Image

--- src/components/banner/index.js
+++ src/components/banner/index.js
@@ -10,14 +10,17 @@ export default function Banner({ medias }) {
 
     return <div className="flex flex-col space-y-2 py-16 md:space-y-4 
         lg:h-[65vh] lg:justify-end lg:pb-12 lg:pl-24">
-        <div className="absolute top-0 left-0 h-[95hv] w-screen -z-10" style={{position:"absolute" }}>
+        <div className="absolute top-0 left-0 h-[95hv] w-screen -z-10" style={{ height: '25%', width: '100%' }}>
             <Image
                 src={`${baseUrl}/${createRandomMedia?.backdrop_path || createRandomMedia?.poster_path}`}
                 alt="Banner"
                 fill priority
-                object-fit="cover"                
-            />
-            <div className="absolute w-full h-32 bg-gradient-to-t from-gray-100 to-transparent bottom-0 z-20" />
+                sizes="(max-width: 100%) 100vw, (max-width: 100%) 50vw, 800px"
+                objectfit="cover"/>
+            <div className="absolute w-full h-32 bg-gradient-to-t from-gray-100 to-transparent bottom-0 z-20"/>
         </div>
+        <h1 className="text-2x1 md:text-4x1 lg:text-7x1 font-bold">
+            {createRandomMedia?.title || createRandomMedia?.name || createRandomMedia?.original_name}
+        </h1>
     </div>
 }
