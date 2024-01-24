5f7a046f415b3e3405985acd17fb18a4f40bcaf9
Alejandro Zepeda <alexzepeda07@hotmail.com>
2024-01-24 00:09:16 -0600

    Implementation questions from db in unauth-page

---
+++ src/app/api/questions/get-all-questions/route.js
@@ -0,0 +1,31 @@
+import connectMongoDB from "@/database";
+import { NextResponse } from "next/server";
+import Question from "@/models/Question";
+
+export const dynamic = "force-dynamic";
+
+export async function GET(request) {
+    try {
+        await connectMongoDB();
+        const getAllQuestions = await Question.find();
+        
+        if (getAllQuestions) {
+            return NextResponse.json({
+                success: true,
+                data: getAllQuestions,
+            })
+        } else {
+            return NextResponse.json({
+                success: false,
+                message: "Something went wrong!",
+            });
+        }
+
+    } catch (e) {
+        console.log(e);
+        return NextResponse.json({
+            success: false,
+            message: "Something went wrong!",
+        });
+    }
+}
--- src/components/unauth-page/index.js
+++ src/components/unauth-page/index.js
@@ -5,45 +5,17 @@ import { motion } from "framer-motion";
 import { PlusIcon } from "@heroicons/react/24/outline";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
+import { getAllQuestions } from "@/utils";
 
-const questions = [
-    {
-        key: "kq-1",
-        ques: "What is Netflix?",
-        ans: `Netflix is a streaming service that offers a wide variety of award-winning TV shows, movies, anime, documentaries and more – on thousands of internet-connected devices. You can watch as much as you want, whenever you want, without a single ad – all for one low monthly price. There's always something new to discover, and new TV shows and movies are added every week!`,
-    },
-    {
-        key: "kq-2",
-        ques: "How much does Netflix cost",
-        ans: `Watch Netflix on your smartphone, tablet, Smart TV, laptop, or streaming device, all for one fixed monthly fee. Plans range from ₹ 149 to ₹ 649 a month. No extra costs, no contracts.`,
-    },
-    {
-        key: "kq-3",
-        ques: "What can I watch on Netflix?",
-        ans: `Watch anywhere, anytime. Sign in with your Netflix account to watch instantly on the web at netflix.com from your personal computer or on any internet-connected device that offers the Netflix app, including smart TVs, smartphones, tablets, streaming media players and game consoles.
-
-    You can also download your favourite shows with the iOS, Android, or Windows 10 app. Use downloads to watch while you're on the go and without an internet connection. Take Netflix with you anywhere.`,
-    },
-    {
-        key: "kq-4",
-        ques: "How do I cancel?",
-        ans: `Netflix is flexible. There are no annoying contracts and no commitments. You can easily cancel your account online in two clicks. There are no cancellation fees – start or stop your account anytime.`,
-    },
-    {
-        key: "kq-5",
-        ques: "What can I watch on Netflix?",
-        ans: `Netflix has an extensive library of feature films, documentaries, TV shows, anime, award-winning Netflix originals, and more. Watch as much as you want, anytime you want.`,
-    },
-    {
-        key: "kq-6",
-        ques: "Is Netflix good for kids?",
-        ans: `The Netflix Kids experience is included in your membership to give parents control while kids enjoy family-friendly TV shows and films in their own space.
-
-Kids profiles come with PIN-protected parental controls that let you restrict the maturity rating of content kids can watch and block specific titles you don’t want kids to see.`,
-    },
-];
+let questions = [];
 
 function UnauthBanner({ router }) {
+    async function getAllMedias() {
+        questions = await getAllQuestions();
+    }
+
+    getAllMedias();
+    
     return (
         <div className="h-[65vh] sm:h-[90vh] xl:h-[95vh] bg-cover bg-no-repeat bg-[url('https://assets.nflxext.com/ffe/siteui/vlv3/84526d58-475e-4e6f-9c81-d2d78ddce803/e3b08071-f218-4dab-99a2-80315f0922cd/LK-en-20221228-popsignuptwoweeks-perspective_alpha_website_small.jpg')] border-b-8 border-gray-800  ">
             <div className="bg-black bg-opacity-70 h-[100vh]">
@@ -90,8 +62,7 @@ export default function UnauthPage() {
         <motion.div
             initial={{ opacity: 0 }}
             whileInView={{ opacity: 1 }}
-            viewport={{ once: true }}
-        >
+            viewport={{ once: true }}>
             <main>
                 <div className="bg-[#000000]">
                     <UnauthBanner router={router} />
@@ -104,12 +75,12 @@ export default function UnauthPage() {
                                 <div className="flex flex-col gap-3" key={item.key}>
                                     <div onClick={() => setShowCurrentAns(showCurrentAns === index ? null : index)}
                                         className="flex justify-between p-3 lg:p-5 mt-2 bg-[#303030] cursor-pointer">
-                                        <h2>{item.ques}</h2>
+                                        <h2>{item.question}</h2>
                                         <PlusIcon className="h-7 w-7" color="white" />
                                     </div>
                                     {showCurrentAns === index && (
                                         <div className="p-3 lg:p-5 mt-2 bg-[#303030] cursor-pointer">
-                                            {item.ans}
+                                            {item.answer}
                                         </div>
                                     )}
                                 </div>
