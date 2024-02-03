e0c6d225687640f9dad005e9f19dc8b584b6545f
Alejandro Zepeda <alexzepeda07@hotmail.com>
2024-02-02 20:12:05 -0600

    Add origin request mongo connection to tv page

--- src/app/tv/page.js
+++ src/app/tv/page.js
@@ -15,6 +15,7 @@ export default function TV() {
 
     useEffect(() => {
         async function getAllMedias() {
+            const origin = "TV-getAllMedias/useEffect";
             const actionAdventure = await getTVorMoviesByGenre("tv", 10759);
             const crime = await getTVorMoviesByGenre("tv", 80);
             const comedy = await getTVorMoviesByGenre("tv", 35);
@@ -25,7 +26,7 @@ export default function TV() {
             const war = await getTVorMoviesByGenre("tv", 10768);
             const western = await getTVorMoviesByGenre("tv", 37);
             const dramaMovies = await getTVorMoviesByGenre("tv", 18);
-            const allfavorites = await getAllfavorites(session?.user?.uid, loggedInAccount?._id);
+            const allfavorites = await getAllfavorites(session?.user?.uid, loggedInAccount?._id, origin);
 
             setMediaData(
                 [
