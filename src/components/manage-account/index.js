e51195c0695eadacae8039f6b381750366d5e0c2
Alejandro Zepeda <alexzepeda07@hotmail.com>
2024-02-04 23:23:45 -0600

    Fix Error: can be escaped react/no-unescaped-entities

--- src/components/manage-account/index.js
+++ src/components/manage-account/index.js
@@ -121,9 +121,9 @@ export default function ManageAccounts() {
 
     return <div className="min-h-screen flex justify-center flex-col items-center relative">
         <div className="flex justify-center flex-col items-center">
-            <h1 className="text-white font-bold text-[54px] my-[36px]">
-                Who's Watching?
-            </h1>
+        <h1 className="text-white font-bold text-[54px] my-[36px]">
+            Who&apos;s Watching?
+        </h1>
             <ul className="flex p-0 my-[25px]">
                 {
                     accounts && accounts.length ?
